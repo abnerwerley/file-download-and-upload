33322ae733f3fa9810ed1af99519c27f9e0faa58
Abner Werley <abnerwerley77@gmail.com>
2022-12-08 22:35:14 -0300

    :poop: @abnerwerley [saving swagger configuration test] ]

--- src/main/java/com/file/Application.java
+++ src/main/java/com/file/Application.java
@@ -2,12 +2,15 @@ package com.file;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+//@EnableWebMvc
+//@ComponentScan({"com.file.upload.controller , com.file.download.controller", "com.file.upload.service, com.file.download.service", "controller", "service"})
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
 }
